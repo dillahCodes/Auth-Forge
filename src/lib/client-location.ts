b0a5b976fbf80d0d8738c9361997b4c95809ec3f
DillahCodes <abijuniansyah@gmail.com>
2025-12-27 01:27:48 +0700

    refactor: update getClientLocation

--- src/lib/client-location.ts
+++ src/lib/client-location.ts
@@ -1,18 +1,21 @@
-import { geolocation } from "@vercel/functions";
-
-const toNumberOrNull = (value?: string): number | null => {
+const toNumberOrNull = (value?: string | null): number | null => {
   if (!value) return null;
   const parsed = Number(value);
   return Number.isFinite(parsed) ? parsed : null;
 };
 
-const toStringOrNull = (value?: string): string | null => {
+const toStringOrNull = (value?: string | null): string | null => {
   if (!value) return null;
   return value;
 };
 
 const getClientLocation = (req: Request) => {
-  const { city, country, countryRegion, region, latitude, longitude } = geolocation(req);
+  const countryRegion = req.headers.get("x-vercel-ip-continent");
+  const country = req.headers.get("x-vercel-ip-country");
+  const region = req.headers.get("x-vercel-ip-country-region");
+  const city = req.headers.get("x-vercel-ip-city");
+  const latitude = req.headers.get("x-vercel-ip-latitude");
+  const longitude = req.headers.get("x-vercel-ip-longitude");
 
   const location = {
     city: toStringOrNull(city),
