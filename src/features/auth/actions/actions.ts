36f474af8df119a0c1d56d6079338a3c99fa2e73
DillahCodes <abijuniansyah@gmail.com>
2025-12-27 01:03:54 +0700

    feat: remove unused logout function and related imports from auth actions

--- src/features/auth/actions/actions.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { deleteSession } from "@/features/auth/lib/sessions";
-import { redirect } from "next/navigation";
-
-export async function logout() {
-  // await deleteSession();
-  redirect("/login");
-}
