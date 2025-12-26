6185b325238477e6682059654d663f4c12288416
DillahCodes <abijuniansyah@gmail.com>
2025-12-27 01:04:10 +0700

    feat: refactor session management functions to improve token handling and validation

--- src/features/auth/lib/sessions.ts
+++ src/features/auth/lib/sessions.ts
@@ -1,20 +1,40 @@
 import "server-only";
-import { jwtVerify, SignJWT } from "jose";
+import { expiresInMiliseconds } from "@/helper/expires-in-miliseconds";
+import { errors, JWTPayload, jwtVerify, SignJWT } from "jose";
 import { cookies } from "next/headers";
+
+export type DecryptResult<TPayload extends JWTPayload = JWTPayload> =
+  | { valid: true; payload: TPayload }
+  | { valid: false; reason: "expired" | "invalid" };
+
+export interface RefreshTokenPayload extends JWTPayload {
+  sessionId: string;
+}
+
+export interface AccessTokenPayload extends JWTPayload {
+  userId: string;
+  sessionId: string;
+}
 
 const SESSION_SECRET = process.env.SESSION_SECRET!;
 const SECRET = new TextEncoder().encode(SESSION_SECRET);
 
-export const ACCESS_TOKEN_EXPIRES_SECONDS = 15 * 60; // 15 minutes
-export const ACCESS_TOKEN_EXPIRES_MILLISECONDS = ACCESS_TOKEN_EXPIRES_SECONDS * 1000; // 15 minutes
+// export const ACCESS_TOKEN_EXPIRES_SECONDS = 15 * 60; // 15 minutes
+export const ACCESS_TOKEN_EXPIRES_SECONDS = 1 * 60; // FOR DEBUGGING: 1 minute
 export const REFRESH_TOKEN_EXPIRES_SECONDS = 60 * 60 * 24 * 7; // 7 days
-export const REFRESH_TOKEN_EXPIRES_MILLISECONDS = REFRESH_TOKEN_EXPIRES_SECONDS * 1000; // 7 days
+// export const REFRESH_TOKEN_EXPIRES_SECONDS = 2 * 60; // FOR DEBUGGING: 2 minutes
 
-export async function signAccessToken({ userId, sessionId }: { userId: string; sessionId: string }) {
+export async function signAccessToken({
+  userId,
+  sessionId,
+}: {
+  userId: string;
+  sessionId: string;
+}) {
   return new SignJWT({ userId, sessionId })
     .setProtectedHeader({ alg: "HS256" })
     .setIssuedAt()
-    .setExpirationTime("15m")
+    .setExpirationTime(expiresInMiliseconds(ACCESS_TOKEN_EXPIRES_SECONDS))
     .sign(SECRET);
 }
 
@@ -22,7 +42,7 @@ export async function signRefreshToken({ sessionId }: { sessionId: string }) {
   return new SignJWT({ sessionId })
     .setProtectedHeader({ alg: "HS256" })
     .setIssuedAt()
-    .setExpirationTime("7d")
+    .setExpirationTime(expiresInMiliseconds(REFRESH_TOKEN_EXPIRES_SECONDS))
     .sign(SECRET);
 }
 
@@ -41,13 +61,16 @@ export async function deleteSession() {
   resultCookie.delete("refresh_token");
 }
 
-export async function decrypt(session: string | undefined = "") {
+export async function decrypt<TPayload extends JWTPayload = JWTPayload>(
+  token?: string
+): Promise<DecryptResult<TPayload>> {
+  if (!token) return { valid: false, reason: "invalid" };
+
   try {
-    const { payload } = await jwtVerify(session, SECRET, {
-      algorithms: ["HS256"],
-    });
-    return payload;
-  } catch {
-    console.log("failed to verify session");
+    const { payload } = await jwtVerify(token, SECRET);
+    return { valid: true, payload: payload as TPayload };
+  } catch (error) {
+    if (error instanceof errors.JWTExpired) return { valid: false, reason: "expired" };
+    return { valid: false, reason: "invalid" };
   }
 }
