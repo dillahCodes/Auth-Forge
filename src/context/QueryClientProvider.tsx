18c8304cc30f098e592308b857827c085a36dc3c
DillahCodes <abijuniansyah@gmail.com>
2025-12-27 01:02:03 +0700

    feat: configure QueryClient with default options for query behavior

--- src/context/QueryClientProvider.tsx
+++ src/context/QueryClientProvider.tsx
@@ -2,7 +2,15 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      retry: 0, // Disable automatic retries, we using axios-retry instead
+    },
+  },
+});
 
 export default function QueryProvider({ children }: { children: React.ReactNode }) {
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
