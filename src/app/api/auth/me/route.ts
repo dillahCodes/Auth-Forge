c81b93571ef8a7adae6a4c1ad5981b52469c18d5
DillahCodes <abijuniansyah@gmail.com>
2025-12-27 01:09:10 +0700

    refactor(auth): update me endpoint to use access token guard

--- src/app/api/auth/me/route.ts
+++ src/app/api/auth/me/route.ts
@@ -1,14 +1,20 @@
-import requiredAuth from "@/features/auth/guard/require-auth";
+import { AppError } from "@/errors/app-error";
+import requiredAccessToken from "@/features/auth/guard/required-access-token";
 import {
-  AppError,
   errorResponse,
   internalServerError,
   sendSuccess,
 } from "@/helper/response-helper";
+import { prisma } from "@/lib/prisma";
 
 export async function GET(req: Request) {
   try {
-    const user = await requiredAuth(req);
+    const { userId } = await requiredAccessToken(req);
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: { id: true, email: true, name: true },
+    });
     return sendSuccess(user, "Get my user data successfully");
   } catch (error) {
     if (error instanceof AppError) return errorResponse(error);
