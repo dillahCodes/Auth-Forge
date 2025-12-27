97c2812cba3dc5c4fdbe63e55f90905e75aec3b6
DillahCodes <abijuniansyah@gmail.com>
2025-12-27 22:59:16 +0700

    fix: unknown device

--- src/lib/parse-device.ts
+++ src/lib/parse-device.ts
@@ -4,7 +4,7 @@ export function parseDevice(userAgent?: string | null): string {
   if (!userAgent) return "Unknown Device";
 
   const parser = new UAParser(userAgent);
-  const deviceType = parser.getDevice().type ?? "Unknown Device";
+  const deviceType = parser.getDevice().type;
   const os = parser.getOS().name ?? "Unknown OS";
   const browser = parser.getBrowser().name ?? "Unknown Browser";
 
