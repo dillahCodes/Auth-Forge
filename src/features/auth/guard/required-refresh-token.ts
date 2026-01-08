1a14455e273e84c6ece847cdb843d455adf895b1
DillahCodes <abijuniansyah@gmail.com>
2026-01-09 02:09:33 +0700

    feat: add analyzeSessionRisk function

--- src/features/auth/guard/required-refresh-token.ts
+++ src/features/auth/guard/required-refresh-token.ts
@@ -1,6 +1,14 @@
 import { AuthSessionRevoked, AuthUnauthorized } from "@/errors/auth-error";
-import { prisma } from "@/lib/prisma";
-import { decrypt, getCookie, type RefreshTokenPayload } from "../lib/sessions";
+import {
+  deleteAllSessionByUserId,
+  getSessionbySessionid,
+} from "../helper/sessions-helper";
+import {
+  decrypt,
+  deleteSession,
+  getCookie,
+  type RefreshTokenPayload,
+} from "../lib/sessions";
 
 type RequiredRefreshTokenResult = {
   sessionId: string;
@@ -10,25 +18,24 @@ type RequiredRefreshTokenResult = {
 export default async function requiredRefreshToken(
   req: Request
 ): Promise<RequiredRefreshTokenResult> {
-  // check if the refresh token is present
   const refreshToken = getCookie(req, "refresh_token");
   if (!refreshToken) throw new AuthUnauthorized();
 
-  // check if the refresh token is valid
   const result = await decrypt<RefreshTokenPayload>(refreshToken);
   if (!result.valid) throw new AuthUnauthorized();
 
-  // check if the sessionId is valid
   const { sessionId } = result.payload;
   if (!sessionId) throw new AuthUnauthorized();
 
-  // Find the session in the database
-  const session = await prisma.sessions.findFirst({
-    where: { id: sessionId, revoked: false },
-    select: { id: true, userId: true },
-  });
+  const session = await getSessionbySessionid(sessionId);
+  if (!session) throw new AuthUnauthorized();
 
-  if (!session) throw new AuthSessionRevoked();
+  const isReuseTokenAttempt = session.revoked || session.replacedBy;
+  if (isReuseTokenAttempt) {
+    await deleteAllSessionByUserId(session.userId);
+    await deleteSession();
+    throw new AuthSessionRevoked();
+  }
 
   return { sessionId: session.id, userId: session.userId };
 }
