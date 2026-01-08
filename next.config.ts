e3a4eafa45b32613335f740fd2d0e3e1250c5dff
DillahCodes <abijuniansyah@gmail.com>
2026-01-08 22:16:31 +0700

    feat: add api header policy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,30 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["http://localhost:3000", "*.ngrok-free.dev"],
+  headers: () => [
+    {
+      source: "/api/:path*",
+      headers: [
+        {
+          key: "Access-Control-Allow-Credentials",
+          value: process.env.ACCESS_CONTROL_ALLOW_CREDENTIALS!,
+        },
+        {
+          key: "Access-Control-Allow-Origin",
+          value: process.env.ACCESS_CONTROL_ALLOW_ORIGIN!,
+        },
+        {
+          key: "Access-Control-Allow-Methods",
+          value: process.env.ACCESS_CONTROL_ALLOW_METHODS!,
+        },
+        {
+          key: "Access-Control-Allow-Headers",
+          value: process.env.ACCESS_CONTROL_ALLOW_HEADERS!,
+        },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;
