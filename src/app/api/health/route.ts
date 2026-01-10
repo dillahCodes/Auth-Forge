27d29c0c02a3bfb45045f0dde890cd05a3890d1b
DillahCodes <abijuniansyah@gmail.com>
2026-01-11 04:55:31 +0700

    refactor: remove unused code

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -4,28 +4,6 @@ import {
   internalServerError,
   sendSuccess,
 } from "@/helper/response-helper";
-import crypto from "crypto";
-import { sha256 } from "js-sha256";
-
-export const POW = {
-  solve: (nonce: string, difficulty: number) => {
-    const target = "0".repeat(difficulty);
-    let counter = 0;
-
-    while (true) {
-      const hash = sha256(nonce + counter);
-      if (hash.startsWith(target)) return counter.toString();
-      counter++;
-    }
-  },
-
-  verify: (nonce: string, proof: string, difficulty: number) => {
-    const target = "0".repeat(difficulty);
-    const result = sha256(nonce + proof);
-    const isValid = result.startsWith(target);
-    return isValid;
-  },
-};
 
 export async function GET(_req: Request) {
   try {
