d7b27dd27cf711626b08346593160a38df2d666a
DillahCodes <abijuniansyah@gmail.com>
2026-01-11 11:11:44 +0700

    fix: displayname validation

--- src/features/auth/schemas/registerSchema.ts
+++ src/features/auth/schemas/registerSchema.ts
@@ -1,7 +1,14 @@
 import z from "zod";
 
 export const registerSchema = z.object({
-  name: z.string().trim().max(20, { message: "Name must be at most 20 characters long" }),
+  name: z
+    .string()
+    .trim()
+    .min(5, { message: "Name must be at least 5 characters long" })
+    .max(20, { message: "Name must be at most 20 characters long" }),
   email: z.email({ message: "Invalid email address" }).trim(),
-  password: z.string().min(8, { message: "Password must be at least 8 characters long" }).trim(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .trim(),
 });
