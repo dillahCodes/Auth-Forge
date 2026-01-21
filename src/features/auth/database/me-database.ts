cfc29d97c857a678e68c48cd2157dca8de4147dd
DillahCodes <abijuniansyah@gmail.com>
2026-01-21 08:03:11 +0700

    feat: remove unused database

--- src/features/auth/database/me-database.ts
+++
@@ -1,6 +0,0 @@
-const getMyUserData = async (userId: string) => {
-  return await prisma.user.findUnique({
-    where: { id: userId },
-    select: { id: true, email: true, name: true, verifiedAt: true },
-  });
-};
