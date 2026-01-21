896ba0e824c0fe5c0fb9d39c01c3ee877298db31
DillahCodes <abijuniansyah@gmail.com>
2026-01-21 08:00:01 +0700

    feat: add health endpoint

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -4,10 +4,22 @@ import {
   internalServerError,
   sendSuccess,
 } from "@/helper/response-helper";
+import { getClientInfo } from "@/lib/client-info";
+import { geoVercel } from "@/lib/geolocation/geo-vercel";
 
-export async function GET(_req: Request) {
+export async function GET(req: Request) {
   try {
-    // const clientInfo = getClientInfo(req);
+    const clientInfo = getClientInfo(req);
+    const geolocation = geoVercel(req, clientInfo.ip as string);
+
+    // const whoisAPi = await whoisGeoLocation(clientInfo.ip as string);
+    // console.log("whoisAPi", whoisAPi);
+
+    // const data = {
+    //   ip: clientInfo.ip,
+    //   ...geolocation,
+    // };
+    // console.log("Geolocation:", data);
 
     // const resultRateLimiter = await rateLimiterFixedWindow({
     //   key: `ip:${clientInfo.ip}:ua:${clientInfo.userAgent}`,
@@ -25,7 +37,8 @@ export async function GET(_req: Request) {
 
     // if (!resultRateLimiter.isAllowed) throw new ToManyRequests();
 
-    return sendSuccess(null, "OK");
+    // return sendSuccess(whoisAPi, "OK");
+    return sendSuccess(geolocation, "OK");
   } catch (error) {
     if (error instanceof AppError) return errorResponse(error);
     return internalServerError(error);
