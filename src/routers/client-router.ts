268cd78bc789e4d462465be9687ee1eb740bc155
DillahCodes <abijuniansyah@gmail.com>
2026-01-21 07:30:44 +0700

    feat: add client router

--- src/routers/client-router.ts
+++ src/routers/client-router.ts
@@ -1,7 +1,27 @@
-export enum RouterUrls {
+export enum ClientRouters {
   LOGIN = "/login",
   REGISTER = "/register",
+  FORGOT_PASSWORD = "/forgot-password",
+  FORGOT_PASSWORD_VERIFY = "/forgot-password/verify",
+
   PROFILE = "/profile",
+  DASHBOARD = "/dashboard",
+
   SESSION_REVOKED = "/session-revoked",
   UNAUTHORIZED = "/unauthorized",
+
+  VERIFY_EMAIL = "/verify-email",
 }
+
+export const ClientPrivateRoutes = [
+  ClientRouters.DASHBOARD,
+  ClientRouters.PROFILE,
+  ClientRouters.VERIFY_EMAIL,
+];
+
+export const AuthRoutes = [
+  ClientRouters.LOGIN,
+  ClientRouters.REGISTER,
+  ClientRouters.FORGOT_PASSWORD,
+  ClientRouters.FORGOT_PASSWORD_VERIFY,
+];
