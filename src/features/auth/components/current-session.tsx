84a98a3b8748a35a3d07f26945830004d4a6c80a
DillahCodes <abijuniansyah@gmail.com>
2026-01-31 08:13:51 +0700

    feat: update user info  component

--- src/features/auth/components/current-session.tsx
+++ src/features/auth/components/current-session.tsx
@@ -1,6 +1,8 @@
 import { Activity } from "react";
 import { Session } from "../types/sessions";
 import iso3166 from "iso-3166-2";
+import { useLogout } from "../hooks/use-logout";
+import { Button } from "@/shared/components/ui/button";
 
 export default function CurrentSession({
   currentSession,
@@ -9,6 +11,8 @@ export default function CurrentSession({
   currentSession: Session | undefined;
   isLoading: boolean;
 }) {
+  const { mutate: logout, isPending } = useLogout();
+
   const { location } = currentSession || {};
   const { city, country, countryRegion } = location || {};
 
@@ -22,23 +26,25 @@ export default function CurrentSession({
     <Activity name="Current Session" mode={currentSession ? "visible" : "hidden"}>
       <section className="border-2 p-3 shadow-strong">
         <h2 className="font-bold mb-2">Current Device</h2>
-        <div className="flex items-center">
-          <span className="w-6 h-6 bg-info rounded-full border-2 border-dark" />
-          <div className="text-sm border border-white p-2">
-            <p className="font-semibold">{currentSession?.device}</p>
-            <p>
-              <Activity name="Current Session" mode={decodeCty ? "visible" : "hidden"}>
-                <span>{decodeCty || "Unknown City"}</span>,{" "}
-              </Activity>
-              <Activity
-                name="Current Session"
-                mode={isoCountryRegion ? "visible" : "hidden"}
-              >
-                <span>{isoCountryRegion || "Unknown Country Region"}</span>,{" "}
-              </Activity>
-              <span>{isoCountry || "Unknown Country"}</span>
-            </p>
+        <div className="w-full flex items-center justify-between">
+          <div className="flex items-center">
+            <span className="w-6 h-6 bg-info rounded-full border-2 border-dark" />
+            <div className="text-sm border border-white p-2">
+              <p className="font-semibold">{currentSession?.device}</p>
+              <p>
+                <Activity name="Current Session" mode={decodeCty ? "visible" : "hidden"}>
+                  <span>{decodeCty || "Unknown City"}</span>,{" "}
+                </Activity>
+                <Activity name="Current Session" mode={isoCountryRegion ? "visible" : "hidden"}>
+                  <span>{isoCountryRegion || "Unknown Country Region"}</span>,{" "}
+                </Activity>
+                <span>{isoCountry || "Unknown Country"}</span>
+              </p>
+            </div>
           </div>
+          <Button variant="danger" onClick={() => logout()} disabled={isPending} className="font-bold">
+            {isPending ? "Logging out..." : "Logout"}
+          </Button>
         </div>
       </section>
     </Activity>
--- src/features/auth/components/user-info.tsx
+++ src/features/auth/components/user-info.tsx
@@ -1,46 +1,76 @@
-import { Activity } from "react";
-import { useLogout } from "../hooks/use-logout";
-import { useMe } from "../hooks/use-me";
-import Link from "next/link";
 import { ClientRouters } from "@/routers/client-router";
+import { Button } from "@/shared/components/ui/button";
+import Link from "next/link";
+import { Activity } from "react";
+import { FaCircleCheck } from "react-icons/fa6";
+import { LuPencil } from "react-icons/lu";
+import { useMe } from "../hooks/use-me";
+import { useRouter } from "next/navigation";
 
 export default function UserInfo() {
+  const router = useRouter();
   const { data: userData, isLoading } = useMe();
-  const { mutate: logout, isPending } = useLogout();
+
+  const handleGoToEdit = () => {
+    router.push(ClientRouters.EDIT_PROFILE);
+  };
 
   if (isLoading) return <UserInfoLoading />;
 
   return (
-    <section className="border-2 shadow-strong p-3 ">
-      <h2 className="font-bold mb-4">User Info</h2>
-      <p>Name: {userData?.data?.name}</p>
-      <div className="flex items-center gap-1">
-        <p>Email: {userData?.data?.email}</p>
-        <Activity
-          name="Email Verified"
-          mode={userData?.data?.verifiedAt ? "visible" : "hidden"}
-        >
-          <p className="text-sm text-gray-500">(Email verified)</p>
-        </Activity>
-        <Activity
-          name="Email Verified"
-          mode={!userData?.data?.verifiedAt ? "visible" : "hidden"}
-        >
-          <Link
-            href={`${ClientRouters.VERIFY_EMAIL}?redirect=${window.location.href}`}
-            className="text-sm text-blue-500 underline cursor-pointer"
-          >
-            (verify email)
-          </Link>
-        </Activity>
+    <section className="border-2 shadow-strong h-64">
+      <div className="w-full grid relative grid-row-4 grid-cols-1 h-full">
+        {/* layer  */}
+        <div className="border-dark border-t-0 h-full border-2 bg-pink-300 border-x-0" />
+        <div className="border-dark relative border-2 border-t-0 h-full border-x-0 row-span-2" />
+
+        {/* profile content */}
+        <div className="w-full h-full absolute grid grid-rows-3 grid-cols-1 z-10">
+          {/* profile */}
+          <div className=" row-span-2 mx-auto m-4 mb-0 sm:mt-4 sm:ml-4 w-fit h-fit">
+            <div className="relative w-36">
+              {/* avatar */}
+              <div className="w-32 border-black flex items-center justify-center text-4xl capitalize text-dark-2 aspect-square border-2 rounded-full bg-info">
+                <p>{userData?.data?.name[0]}</p>
+              </div>
+              {/* edit */}
+              <Button
+                onClick={handleGoToEdit}
+                variant="outline"
+                className="absolute aspect-square w-8 top-0 right-4 bg-amber-800 hover:bg-amber-600 rounded-full border-2 border-dark flex items-center justify-center"
+              >
+                <span className="flex items-center justify-center text-white">
+                  <LuPencil />
+                </span>
+              </Button>
+            </div>
+          </div>
+
+          <div className="h-full row-span-1">
+            <div className="mx-4">
+              {/* name */}
+              <h1 className="font-bold text-lg text-center sm:text-left">{userData?.data?.name}</h1>
+
+              {/* email */}
+              <p className="text-dark-6 flex items-center justify-center sm:justify-normal gap-2">
+                <span className="truncate">{userData?.data?.email}</span>
+
+                <Activity name="Verified" mode={userData?.data?.verifiedAt ? "visible" : "hidden"}>
+                  <span className="text-info">
+                    <FaCircleCheck />
+                  </span>
+                </Activity>
+
+                <Activity name="Verified" mode={!userData?.data?.verifiedAt ? "visible" : "hidden"}>
+                  <Link href={ClientRouters.VERIFY_EMAIL}>
+                    <span className="text-dark-6 text-xs underline italic">Verify Email</span>
+                  </Link>
+                </Activity>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <button
-        onClick={() => logout()}
-        disabled={isPending}
-        className="w-full  py-2 mt-3 cursor-pointer border-2 border-dark text-dark-2 font-bold bg-danger hover:opacity-50 transition-all duration-300 disabled:opacity-50"
-      >
-        {isPending ? "Logging out..." : "Logout"}
-      </button>
     </section>
   );
 }
