9152e16d192309a4cb0271ae45145f3b898d3163
DillahCodes <abijuniansyah@gmail.com>
2026-02-17 18:13:48 +0700

    feat: add EmailChangeRequest interface and update User type to include pendingEmailChange; enhance error codes in ApiResponse

--- src/features/auth/types/user.ts
+++ src/features/auth/types/user.ts
@@ -1,7 +1,12 @@
+import { EmailChangeRequest } from "../../../../prisma/generated/client";
+
 export interface User {
   id: string;
   email: string;
+  pendingEmailChange: EmailChangeRequest | null;
   name: string;
   password: string;
   verifiedAt: Date | null;
 }
+
+export type UserData = Omit<User, "password">;
