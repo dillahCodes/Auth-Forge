ca375d26b6998cae99a4ac40e35333774fea2040
DillahCodes <abijuniansyah@gmail.com>
2026-02-17 18:30:39 +0700

    feat: enhance user data retrieval with session validation and mapping

--- src/features/auth/controllers/me.controller.ts
+++ src/features/auth/controllers/me.controller.ts
@@ -1,13 +1,17 @@
 import { sendSuccess } from "@/shared/utils/response-helper";
 import { AccessTokenHttp } from "../http/access-token.http";
+import { MeMapping, meMapping } from "../mapping/me.mapping";
 import { MeService } from "../services/me.service";
+import { SessionService } from "../services/session.service";
 import { CreateController } from "./create.controller";
 
 export const MeController = {
   getMe: CreateController.create(async (req: Request) => {
-    const { userId } = await AccessTokenHttp.requiredAccessToken(req);
-    const user = await MeService.getMe(userId);
+    const { userId, sessionId } = await AccessTokenHttp.requiredAccessToken(req);
+    await SessionService.validateSessionForAccessToken(sessionId);
 
-    return sendSuccess(user, "Get my user data successfully");
+    const user = await MeService.getMe(userId);
+    const mappedUserData = meMapping(user as MeMapping);
+    return sendSuccess(mappedUserData, "Get my user data successfully");
   }),
 };
--- src/features/auth/services/me.service.ts
+++ src/features/auth/services/me.service.ts
@@ -1,8 +1,11 @@
+import { EmailChangeRequestRepository } from "../repositories/email-change-request.repository";
 import { UserRepository } from "../repositories/user.repositoriy";
 
 export const MeService = {
   async getMe(userId: string) {
     const user = await UserRepository.getById(userId);
-    return user;
+    const pendingEmailChange = await EmailChangeRequestRepository.findPendingByUserId(userId);
+
+    return { user, pendingEmailChange };
   },
 };
