bd056f2bc5e022921d41bff6de47a2fddadc0ac1
DillahCodes <abijuniansyah@gmail.com>
2026-02-17 18:15:12 +0700

    feat: add StatusBadge and IconWithText components; enhance Button component with icon support

--- src/shared/components/ui/button.tsx
+++ src/shared/components/ui/button.tsx
@@ -1,21 +1,36 @@
+import { Activity } from "react";
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   variant: "info" | "warning" | "danger" | "success" | "text" | "outline";
+  iconLeft?: React.ReactNode;
+  iconRight?: React.ReactNode;
 }
 
-export function Button({ children, variant, ...props }: ButtonProps) {
+export function Button({ children, variant, iconLeft, iconRight, ...props }: ButtonProps) {
   const styles = {
-    info: "bg-info text-dark-2 cursor-pointer border-2 border-dark p-2 disabled:bg-info/60 hover:bg-info/80",
-    warning: "bg-warning text-black cursor-pointer border-2 border-dark p-2 disabled:bg-warning/60 hover:bg-warning/80",
-    danger: "bg-danger text-dark-2 cursor-pointer border-2 border-dark p-2 disabled:bg-danger/60 hover:bg-danger/80",
-    success: "bg-success text-dark-2 cursor-pointer border-2 border-dark p-2 disabled:bg-success/60",
+    info: "bg-info text-dark-2 cursor-pointer border-2 border-dark p-2 px-4 disabled:bg-info/60 hover:bg-info/80",
+    warning:
+      "bg-warning text-black cursor-pointer border-2 border-dark p-2 px-4 disabled:bg-warning/60 hover:bg-warning/80",
+    danger:
+      "bg-danger text-dark-2 cursor-pointer border-2 border-dark p-2 px-4 disabled:bg-danger/60 hover:bg-danger/80",
+    success: "bg-success text-dark-2 cursor-pointer border-2 border-dark p-2 px-4 disabled:bg-success/60",
     text: "bg-transparent text-black cursor-pointer disabled:text-dark-5",
-    outline: "text-black cursor-pointer border-2 border-dark disabled:text-dark-5",
+    outline: "text-black cursor-pointer border-2 border-dark disabled:text-dark-5 px-4 py-2",
   };
 
   return (
-    <button {...props} className={`text-sm ${styles[variant]} ${props.className}`}>
+    <button
+      {...props}
+      className={`text-sm ${iconLeft || (iconRight && "flex items-center gap-2")} ${styles[variant]} ${props.className}`}
+    >
+      <Activity name="button icon left" mode={!iconLeft ? "hidden" : "visible"}>
+        {iconLeft}
+      </Activity>
       {children}
+      <Activity name="button icon right" mode={!iconRight ? "hidden" : "visible"}>
+        {iconRight}
+      </Activity>
     </button>
   );
 }
