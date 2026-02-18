ffefdae743cbdaf1474af551c0c372d5cc397d0b
DillahCodes <abijuniansyah@gmail.com>
2026-02-19 03:22:55 +0700

    feat: integrate 2FA status handling in EditEmail component for email change requests

--- src/features/auth/components/profile/edit/edit-email.tsx
+++ src/features/auth/components/profile/edit/edit-email.tsx
@@ -1,3 +1,5 @@
+import { useTwoFaStatus } from "@/features/auth/hooks/use-2fa-status";
+import { StatusTwoFaToken } from "@/features/auth/types/2fa";
 import { StatusBadge } from "@/shared/components/ui/badge";
 import { Button } from "@/shared/components/ui/button";
 import { Form } from "@/shared/components/ui/form/form";
@@ -13,12 +15,12 @@ import { AxiosError } from "axios";
 import { Activity, useEffect, useEffectEvent, useState } from "react";
 import { BsPencilSquare } from "react-icons/bs";
 import { MdOutlineEmail } from "react-icons/md";
+import { EmailChangeRequest } from "../../../../../../prisma/generated/client";
 import { useChangeEmailCancel } from "../../../hooks/use-change-email-cancel";
 import { useChangeEmailRequest } from "../../../hooks/use-change-email-request";
 import { useChangeEmailUpdate } from "../../../hooks/use-change-email-update";
 import { ChangeEmailSchema, ChangeEmailUpdateSchema } from "../../../schemas/account.schema";
 import { useVerifyChangeEmailModal, VerifyChangeEmailConfig } from "../../modal/edit-email-verify.modal";
-import { EmailChangeRequest } from "../../../../../../prisma/generated/client";
 
 interface ChangeEmailProps {
   defaultEmail?: string;
@@ -26,6 +28,10 @@ interface ChangeEmailProps {
 }
 
 type FormMode = "PENDING" | "EDIT" | "PENDING_UPDATE";
+
+type formdataEntries = {
+  [k: string]: FormDataEntryValue;
+} | null;
 
 const TWO_VERIFICATION_CONFIG: TwoFaConfig = { timeoutSeconds: 120, featureKey: "2fa-edit-email", otpLength: 6 };
 const CHANGE_EMAIL_CONFIG: VerifyChangeEmailConfig = {
@@ -36,12 +42,14 @@ const CHANGE_EMAIL_CONFIG: VerifyChangeEmailConfig = {
 
 export default function EditEmail({ defaultEmail, pendingEmailChange }: ChangeEmailProps) {
   const email = defaultEmail as string;
+  const [formMode, setFormMode] = useState<FormMode>("EDIT");
+  const [formdataEntries, setFormDataEntries] = useState<formdataEntries>(null);
 
   const modal = useModal();
   const twoFaModal = useTwoFaModal();
   const verifyChangeEmailModal = useVerifyChangeEmailModal();
 
-  const [formMode, setFormMode] = useState<FormMode>("EDIT");
+  const { data: twoFaStatusData, mutate: twoFaStatusMutate, isPending: isTwoFaStatusPending } = useTwoFaStatus();
 
   const {
     mutate: requestChangeEmail,
@@ -49,6 +57,7 @@ export default function EditEmail({ defaultEmail, pendingEmailChange }: ChangeEm
     data: dataRequestChangeEmail,
     reset: resetRequestChangeEmail,
     error: errorRequestChangeEmail,
+    isPending: isRequestPendingChangeEmail,
   } = useChangeEmailRequest();
 
   const {
@@ -57,6 +66,7 @@ export default function EditEmail({ defaultEmail, pendingEmailChange }: ChangeEm
     data: dataUpdateChangeEmail,
     reset: resetUpdateChangeEmail,
     error: errorUpdateChangeEmail,
+    isPending: isUpdatePendingChangeEmail,
   } = useChangeEmailUpdate();
 
   const {
@@ -65,7 +75,14 @@ export default function EditEmail({ defaultEmail, pendingEmailChange }: ChangeEm
     data: dataCancelChangeEmail,
     reset: resetCancelChangeEmail,
     error: errorCancelChangeEmail,
+    isPending: isCancelPendingChangeEmail,
   } = useChangeEmailCancel();
+
+  const isFormEditAndPendingUpdateLoading = [
+    isRequestPendingChangeEmail,
+    isUpdatePendingChangeEmail,
+    isTwoFaStatusPending,
+  ].some(Boolean);
 
   const axiosErrorRequestChangeEmail = errorRequestChangeEmail as AxiosError<ApiResponse>;
   const axiosErrorUpdateChangeEmail = errorUpdateChangeEmail as AxiosError<ApiResponse>;
@@ -83,27 +100,31 @@ export default function EditEmail({ defaultEmail, pendingEmailChange }: ChangeEm
   const requestChangeEmailMessage = useBuildAxiosError({ errors: requestChangeEmailFlows, resetState });
 
   // DOC: handle form mode
+  //     - PENDING: show pending email change
+  //     - PENDING_UPDATE: show pending email change
+  //     - EDIT: show edit email form
   const handleFormMode = useEffectEvent((mode: FormMode) => {
     setFormMode(mode);
   });
 
   // DOC: handle request update email
-  const handleRequestChangeEmail = (formdata: FormData) => {
-    const payloadEmailChange = { ...Object.fromEntries(formdata.entries()), email } as ChangeEmailSchema;
+  //      - send request to change email and show pending email change
+  const handleRequestChangeEmail = () => {
+    const payloadEmailChange = { ...formdataEntries, email } as ChangeEmailSchema;
     requestChangeEmail(payloadEmailChange);
   };
 
   // DOC: handle update requested email
-  const handleUpdateRequestedEmail = async (formData: FormData) => {
-    const formdataEntries = Object.fromEntries(formData.entries());
+  //      - send request to update pending email
+  const handleUpdateRequestedEmail = async () => {
     const payloadEmailChange = { ...formdataEntries, requestId: pendingEmailChange?.id } as ChangeEmailUpdateSchema;
-
     await updateChangeEmail(payloadEmailChange);
     setFormMode("PENDING");
   };
 
   // DOC: handle cancel change email
-  const handleChangeEmailCancel = () => {
+  //      - send request to cancel pending email
+  const handleCancelChangeEmail = () => {
     modal.open({
       title: "Cancel Email Change",
       content: "Are you sure you want to cancel your email change request?",
@@ -114,32 +135,81 @@ export default function EditEmail({ defaultEmail, pendingEmailChange }: ChangeEm
     });
   };
 
+  // DOC: handle send verification to new email
+  const handleVerifyNewEmail = async () => {
+    const newEmail: string = pendingEmailChange?.newEmail as string;
+    verifyChangeEmailModal.open({ config: CHANGE_EMAIL_CONFIG, sendToEmail: newEmail });
+  };
+
   // DOC: handle submit
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const emailFormData = new FormData(e.currentTarget);
-    const newEmail = emailFormData.get("newEmail")?.toString().trim();
-
-    if (!newEmail) return;
-
-    twoFaModal.open({
-      config: TWO_VERIFICATION_CONFIG,
-      sendToEmail: email,
-      scope: "CHANGE_EMAIL",
-      afterVerify() {
-        formMode.includes("PENDING_UPDATE")
-          ? handleUpdateRequestedEmail(emailFormData)
-          : handleRequestChangeEmail(emailFormData);
-      },
-    });
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+    setFormDataEntries(data);
   };
 
-  // DOC: handle send verification email
-  const handleSendVerificationEmail = async () => {
-    const newEmail: string = pendingEmailChange?.newEmail as string;
-    verifyChangeEmailModal.open({ config: CHANGE_EMAIL_CONFIG, sendToEmail: newEmail });
-  };
+  // DOC: handle form mode decision
+  const handleFormModeDecision = useEffectEvent(() => {
+    if (formMode.includes("PENDING_UPDATE")) {
+      modal.open({
+        title: "Update Pending Email Change",
+        content: "Are you sure you want to update your pending email change?",
+        onConfirm: async () => {
+          await handleUpdateRequestedEmail();
+        },
+      });
+      return;
+    }
+
+    if (formMode.includes("EDIT")) {
+      twoFaStatusMutate({ scope: "CHANGE_EMAIL" });
+      return;
+    }
+  });
+
+  // DOC: handle open two fa modal
+  const handleOpenTwoFaModal = useEffectEvent(() => {
+    if (formMode.includes("EDIT")) {
+      twoFaModal.open({
+        config: TWO_VERIFICATION_CONFIG,
+        sendToEmail: email,
+        scope: "CHANGE_EMAIL",
+        afterVerify() {
+          handleRequestChangeEmail();
+        },
+      });
+    }
+  });
+
+  // DOC: handle direct update or request email change
+  //      - if form mode is EDIT, request change email
+  //      - if form mode is PENDING_UPDATE, update pending email
+  const handleDirectUpdateOrRequestEmailChange = useEffectEvent(() => {
+    formMode.includes("EDIT") && handleRequestChangeEmail();
+    formMode.includes("PENDING_UPDATE") && handleUpdateRequestedEmail();
+  });
+
+  // DOC: effects handle form decision when form data is available
+  useEffect(() => {
+    if (formdataEntries) handleFormModeDecision();
+  }, [formdataEntries]);
+
+  // DOC: open modal or directly send request email change based on 2fa status
+  useEffect(() => {
+    if (!twoFaStatusData) return;
+    const StatusTwoFa = twoFaStatusData.data?.status as StatusTwoFaToken;
+
+    const openModalConditions = [StatusTwoFaToken.INVALID_SCOPE, StatusTwoFaToken.MISSING];
+    const shouldOpenModal = openModalConditions.includes(StatusTwoFa);
+
+    if (shouldOpenModal) handleOpenTwoFaModal();
+
+    const sendRequestChangeCondition = [StatusTwoFaToken.AVAILABLE];
+    const shouldSendRequest = sendRequestChangeCondition.includes(StatusTwoFa);
+    if (shouldSendRequest) handleDirectUpdateOrRequestEmailChange();
+  }, [twoFaStatusData]);
 
   // DOC: effects handle form mode
   useEffect(() => {
@@ -151,21 +221,25 @@ export default function EditEmail({ defaultEmail, pendingEmailChange }: ChangeEm
     <Form className="p-4!" onSubmit={handleSubmit}>
       <h2 className="font-bold mb-2">Email</h2>
 
+      {/* message box */}
       <Activity mode={requestChangeEmailMessage ? "visible" : "hidden"}>
         <MessageBox type={requestChangeEmailMessage?.type as "success" | "error"}>
           {requestChangeEmailMessage?.message}
         </MessageBox>
       </Activity>
 
+      {/* current email with status */}
       <div className="flex gap-4 items-center">
         <IconWithText icon={MdOutlineEmail} text={defaultEmail} />
         <StatusBadge status="verified" />
       </div>
 
+      {/* new email with status */}
       <Activity name="cancel email change" mode={["PENDING"].includes(formMode) ? "visible" : "hidden"}>
         <div className="flex gap-4 items-center">
           <IconWithText icon={MdOutlineEmail} text={pendingEmailChange?.newEmail} />
           <StatusBadge status="pending" />
+
           <Button
             iconRight={<BsPencilSquare />}
             onClick={() => setFormMode("PENDING_UPDATE")}
@@ -177,16 +251,30 @@ export default function EditEmail({ defaultEmail, pendingEmailChange }: ChangeEm
           </Button>
         </div>
 
+        {/* verify new email and cancel change email button */}
         <div className="w-full grid grid-cols-2 gap-4">
-          <Button variant="danger" type="button" className="font-bold" onClick={handleChangeEmailCancel}>
+          <Button
+            variant="danger"
+            type="button"
+            isLoading={isCancelPendingChangeEmail}
+            className="font-bold"
+            onClick={handleCancelChangeEmail}
+          >
             Cancel
           </Button>
-          <Button variant="info" type="button" className=" font-bold" onClick={handleSendVerificationEmail}>
+          <Button
+            variant="info"
+            type="button"
+            disabled={isCancelPendingChangeEmail}
+            className="font-bold"
+            onClick={handleVerifyNewEmail}
+          >
             Verify
           </Button>
         </div>
       </Activity>
 
+      {/* input new email and edit pending email */}
       <Activity name="input new email" mode={["EDIT", "PENDING_UPDATE"].includes(formMode) ? "visible" : "hidden"}>
         <div className="flex gap-4">
           <InputEmail
@@ -196,7 +284,13 @@ export default function EditEmail({ defaultEmail, pendingEmailChange }: ChangeEm
           />
         </div>
 
-        <Button variant="info" type="submit" className="w-full font-bold disabled:opacity-50">
+        <Button
+          isLoading={isFormEditAndPendingUpdateLoading}
+          disabled={isFormEditAndPendingUpdateLoading}
+          variant="info"
+          type="submit"
+          className="w-full font-bold disabled:opacity-50"
+        >
           Edit Email
         </Button>
       </Activity>
