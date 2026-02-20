7fe8f512443f4530f553952342595eca80993eda
DillahCodes <abijuniansyah@gmail.com>
2026-02-20 11:43:12 +0700

    feat: enhance MeService and mapping to include provider in user data retrieval

--- src/features/auth/services/me.service.ts
+++ src/features/auth/services/me.service.ts
@@ -1,11 +1,12 @@
+import { AuthProvider } from "../../../../prisma/generated/enums";
 import { EmailChangeRequestRepository } from "../repositories/email-change-request.repository";
 import { UserRepository } from "../repositories/user.repositoriy";
 
 export const MeService = {
-  async getMe(userId: string) {
-    const user = await UserRepository.getById(userId);
+  async getMe(userId: string, provider: AuthProvider) {
+    const user = await UserRepository.getById({ userId });
     const pendingEmailChange = await EmailChangeRequestRepository.findPendingByUserId(userId);
 
-    return { user, pendingEmailChange };
+    return { user, pendingEmailChange, provider };
   },
 };
