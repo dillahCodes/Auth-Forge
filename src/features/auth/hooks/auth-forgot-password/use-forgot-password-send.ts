1e853c53355ce1aa16cb31107f46333c044fee03
DillahCodes <abijuniansyah@gmail.com>
2026-02-27 16:30:01 +0700

    ref: hooks forgot-password

--- src/features/auth/hooks/auth-forgot-password/use-forgot-password-send.ts
+++ src/features/auth/hooks/auth-forgot-password/use-forgot-password-send.ts
@@ -4,10 +4,11 @@ import { ApiRouters } from "@/routers/api-router";
 import { axiosInstance } from "@/shared/lib/axios/axios";
 import { ApiResponse } from "@/shared/types/response";
 import { useMutation } from "@tanstack/react-query";
+import { ForgotPasswordEmailSchema } from "../../schemas/forgot-password.schema";
 
 export function useForgotPasswordSend() {
   const mutation = useMutation({
-    mutationFn: async (payload: { email: string }) => {
+    mutationFn: async (payload: ForgotPasswordEmailSchema) => {
       const res = await axiosInstance.post(ApiRouters.FORGOT_PASSWORD_SEND, payload, {
         headers: { "Content-Type": "multipart/form-data" },
       });
--- src/features/auth/hooks/auth-forgot-password/use-forgot-password-verify.ts
+++ src/features/auth/hooks/auth-forgot-password/use-forgot-password-verify.ts
@@ -1,15 +1,14 @@
 "use client";
 
+import { ApiRouters } from "@/routers/api-router";
 import { axiosInstance } from "@/shared/lib/axios/axios";
 import { ApiResponse } from "@/shared/types/response";
 import { useMutation } from "@tanstack/react-query";
-import { forgotPasswordSchema } from "../schemas/forgot-password.schema";
-import z from "zod";
-import { ApiRouters } from "@/routers/api-router";
+import { ForgotPasswordSchema } from "../../schemas/forgot-password.schema";
 
 export function useForgotPasswordVerify() {
   const mutation = useMutation({
-    mutationFn: async (payload: z.infer<typeof forgotPasswordSchema>) => {
+    mutationFn: async (payload: ForgotPasswordSchema) => {
       const res = await axiosInstance.post(ApiRouters.FORGOT_PASSWORD_VERIFY, payload, {
         headers: { "Content-Type": "multipart/form-data" },
       });
