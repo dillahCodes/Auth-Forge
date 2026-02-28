cfe58ad9476061594577891194b161d54a50562b
DillahCodes <abijuniansyah@gmail.com>
2026-02-28 23:04:38 +0700

    fix: user profile responsive

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,7 +2,7 @@ import { Header } from "@/shared/components/layout/header";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <section className="w-full flex flex-col gap-8 items-center justify-center">
+    <section className="w-full flex flex-col gap-8 p-3 items-center justify-center">
       <Header />
       {children}
     </section>
