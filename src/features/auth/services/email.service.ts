1f7959aeeca038ce12cd75bca0d3e6017b5fb057
DillahCodes <abijuniansyah@gmail.com>
2026-07-30 16:07:44 +0700

    fix: update email service configuration

--- src/features/auth/services/email.service.ts
+++ src/features/auth/services/email.service.ts
@@ -7,7 +7,7 @@ import { PasswordResetVerifyTemplate } from "../components/template/password-res
 import { VerifyEmailTemplate } from "../components/template/verify-email.template";
 import { EmailChangeRevertTemplate } from "../components/template/email-change-revert.template";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY || "re_dummy");
 
 interface SendEmailParams {
   to: string;
